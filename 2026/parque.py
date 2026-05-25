213812cc0bc9664b56e5e9bbce2b4d8ec69b0016
David Y <davidyudih@gmail.com>
2026-05-25 18:47:38 -0300

    Exercicio ressonancia feito?

---
+++ 2026/parque.py
@@ -0,0 +1,6 @@
+t, n = map(int, input().split())
+tmin = t * 60
+for c in range(n):
+    txt = input()
+    i, d = map(int, input().split())
+    
--- 2026/ressonancia.py
+++ 2026/ressonancia.py
@@ -3,12 +3,15 @@ n = int(input())
 matriz = []
 distancia = []
 linha = []
-for i in range(n):
-    x, y = map(int, input().split())
+for c in range(n):
+    x, y = map(float, input().split())
     linha.append(x)
     linha.append(y)
     matriz.append(linha[:])
     linha.clear()
-    print(f"i = {i}")
-print(matriz)
-print(linha)
+for i in range(len(matriz)):
+    for j in range(len(matriz) - i):
+        d = math.sqrt(pow((matriz[i][0] - matriz[j][0]), 2)+pow((matriz[i][1]-matriz[j][1]),2))
+        if d != 0 and d not in distancia:
+            distancia.append(d)
+print(f"{min(distancia):.4f}")
